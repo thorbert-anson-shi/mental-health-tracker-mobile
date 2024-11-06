8ba9c441185e254928a74f4a1ca25129cb94a43c
thorbert <tobyas2005139@gmail.com>
2024-11-06 15:37:44 +0700

    Refactored tutorial 7

--- lib/screens/menu.dart
+++ lib/screens/menu.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:mental_health_tracker/widgets/mood_card.dart';
+import 'package:mental_health_tracker/widgets/left_drawer.dart';
 
 class MyHomePage extends StatelessWidget {
   final String npm = "2306221900";
@@ -24,6 +26,7 @@ class MyHomePage extends StatelessWidget {
           ),
         ),
       ),
+      drawer: const LeftDrawer(),
       body: Padding(
         padding: const EdgeInsets.all(16.0),
         child: Column(
@@ -89,59 +92,6 @@ class InfoCard extends StatelessWidget {
             const SizedBox(height: 8.0),
             Text(content)
           ],
-        ),
-      ),
-    );
-  }
-}
-
-class ItemHomePage {
-  final String name;
-  final IconData icon;
-
-  ItemHomePage(this.name, this.icon);
-}
-
-class ItemCard extends StatelessWidget {
-  final ItemHomePage item;
-
-  const ItemCard(this.item, {super.key});
-
-  @override
-  Widget build(BuildContext context) {
-    return Material(
-      color: Theme.of(context).colorScheme.secondary,
-      borderRadius: BorderRadius.circular(12.0),
-      child: InkWell(
-        onTap: () {
-          ScaffoldMessenger.of(context)
-            ..hideCurrentSnackBar()
-            ..showSnackBar(
-              SnackBar(
-                content: Text("Kamu telah menekan tombol ${item.name}!"),
-              ),
-            );
-        },
-        child: Container(
-          padding: const EdgeInsets.all(8),
-          child: Center(
-            child: Column(
-              mainAxisAlignment: MainAxisAlignment.center,
-              children: [
-                Icon(
-                  item.icon,
-                  color: Colors.white,
-                  size: 30.0,
-                ),
-                const Padding(padding: EdgeInsets.all(3)),
-                Text(
-                  item.name,
-                  textAlign: TextAlign.center,
-                  style: const TextStyle(color: Colors.white),
-                ),
-              ],
-            ),
-          ),
         ),
       ),
     );
---
+++ lib/widgets/mood_card.dart
@@ -0,0 +1,62 @@
+import 'package:flutter/material.dart';
+import 'package:mental_health_tracker/moodentry_form.dart';
+
+class ItemHomePage {
+  final String name;
+  final IconData icon;
+
+  ItemHomePage(this.name, this.icon);
+}
+
+class ItemCard extends StatelessWidget {
+  final ItemHomePage item;
+
+  const ItemCard(this.item, {super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Material(
+      color: Theme.of(context).colorScheme.secondary,
+      borderRadius: BorderRadius.circular(12.0),
+      child: InkWell(
+        onTap: () {
+          ScaffoldMessenger.of(context)
+            ..hideCurrentSnackBar()
+            ..showSnackBar(
+              SnackBar(
+                content: Text("Kamu telah menekan tombol ${item.name}!"),
+              ),
+            );
+
+          if (item.name == "Tambah Mood") {
+            Navigator.push(
+                context,
+                MaterialPageRoute(
+                    builder: (context) => const MoodEntryFormPage()));
+          }
+        },
+        child: Container(
+          padding: const EdgeInsets.all(8),
+          child: Center(
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                Icon(
+                  item.icon,
+                  color: Colors.white,
+                  size: 30.0,
+                ),
+                const Padding(padding: EdgeInsets.all(3)),
+                Text(
+                  item.name,
+                  textAlign: TextAlign.center,
+                  style: const TextStyle(color: Colors.white),
+                ),
+              ],
+            ),
+          ),
+        ),
+      ),
+    );
+  }
+}
